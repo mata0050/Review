d84fe2a60af066da9d4628ddb2a5d1493daed58c
mata0050 <58061791+mata0050@users.noreply.github.com>
2021-03-07 22:29:36 -0500

    First Commit

---
+++ js/data.js
@@ -0,0 +1,31 @@
+const reviewArray = [
+
+  {
+    profilePic: './images/person-1.jpg',
+    employeeName: 'Susan Smith',
+    job: 'Web Developer',
+    description:`I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry` ,
+  },
+
+  {
+    profilePic: './images/person-2.jpg',
+    employeeName: 'Anna Johnson',
+    job: 'Web Developer',
+    description:`Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.` ,
+  },
+
+  {
+    profilePic: './images/person-3.jpg',
+    employeeName: 'Peter Jones',
+    job: 'Intern',
+    description:`Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.` ,
+  },
+
+  {
+    profilePic: './images/person-4.jpg',
+    employeeName: 'Bill Anderson',
+    job: 'Boss',
+    description:`Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.` ,
+  },
+
+];
---
+++ js/index.js
@@ -0,0 +1,111 @@
+// const card = document.querySelector('.card');
+const dataCard = document.querySelector('.dataCard');
+const btnBack = document.querySelector('#back');
+const btnFront = document.querySelector('#front');
+const btnSurprise = document.querySelector('button');
+const imgContainer = document.querySelector('.imgContainer');
+
+let cardNumber = 0;
+
+/*************************************
+ * Create Elements
+**************************************/
+
+const img = document.createElement('div');
+const h3 = document.createElement('h3');
+const span = document.createElement('span');
+const p = document.createElement('p');
+
+
+/*************************************
+ * Function that creates the review 
+ * card, getting information from
+ * the reviewArray.
+**************************************/
+
+function getCard(){
+
+  img.className = 'img';
+  if(reviewArray[cardNumber].profilePic == './images/person-1.jpg'){
+    img.style.backgroundImage= "url('./images/person-1.jpg')";
+  } 
+  if(reviewArray[cardNumber].profilePic == './images/person-2.jpg'){
+    img.style.backgroundImage= "url('./images/person-2.jpg')";
+  } 
+  if(reviewArray[cardNumber].profilePic == './images/person-3.jpg'){
+    img.style.backgroundImage= "url('./images/person-3.jpg')";
+  } 
+  if(reviewArray[cardNumber].profilePic == './images/person-4.jpg'){
+    img.style.backgroundImage= "url('./images/person-4.jpg')";
+  } 
+ 
+  h3.innerText=reviewArray[cardNumber].employeeName;
+  span.innerText=reviewArray[cardNumber].job;
+  p.innerText=reviewArray[cardNumber].description;
+  imgContainer.appendChild(img);
+  dataCard.appendChild(h3);
+  dataCard.appendChild(span);
+  dataCard.appendChild(p);   
+  
+}
+
+getCard();
+
+
+/***************************************
+ * Random number for Surprise Me Button
+**************************************/
+
+function randomNumber(){
+  return Math.floor(Math.random() * 4);
+}
+
+//**********************************  BUTTONS  *********************** */
+
+
+/***************************************
+ * Looping through the cards 
+ * going forward
+**************************************/
+
+
+btnFront.addEventListener('click',()=>{
+  cardNumber = cardNumber + 1;
+
+  if(cardNumber <= 3){
+   getCard();
+    console.log(cardNumber);
+  }else{
+    cardNumber = 0;
+    getCard();
+  }
+});
+
+
+/***************************************
+ * Looping through the cards 
+ * going backward
+**************************************/
+
+
+btnBack.addEventListener('click',()=>{
+  cardNumber = cardNumber - 1;
+
+  if(cardNumber < 0){
+    cardNumber = 3;
+   getCard();
+  }else{
+    getCard();
+  }
+});
+
+/***************************************
+ *Surprise Button
+**************************************/
+
+
+btnSurprise.addEventListener('click',()=>{
+  cardNumber = randomNumber();
+  getCard();
+});
+
